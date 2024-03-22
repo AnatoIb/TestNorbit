48dbd015faad06ab25f9a2aa9f37425ae3e44bbd
AnatoIb <tolikusa4ev20@gmail.com>
2024-03-22 14:30:03 +0300

    Описана основная логика расчета ИМТ

---
+++ TestNORBIT/IndexCalculation.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNORBIT
+{
+    class IndexCalculation
+    {
+        public void InputData()
+        {
+            bool dataCorrect = false;
+            float weight = 0.0f;
+            float height = 0.0f;
+
+            while (!dataCorrect)
+            {
+                try
+                {
+                    Console.WriteLine("Введите вес тела (в кг): ");
+                    weight = float.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Введите свой рост (в см): ");
+                    height = float.Parse(Console.ReadLine()) / 100;
+
+                    if (weight <= 0 || height <= 0)
+                    {
+                        Console.WriteLine("Введены неверные данные, попробуйте ещё раз\n");
+                    }
+                    else
+                    {
+                        dataCorrect = true;
+                    }
+                } 
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception.Message + "\n");
+                }
+            }
+           
+
+            float bodyMassIndex = (float)(weight / Math.Pow(height, 2));
+
+            string result = "";
+
+            if (bodyMassIndex > 0 && bodyMassIndex < 18.5f)
+            {
+                result = "ИМТ = " + bodyMassIndex + " - Ниже нормального";
+            }
+            else if (bodyMassIndex >= 18.5 && bodyMassIndex < 25)
+            {
+                result = "ИМТ = " + bodyMassIndex + " - Нормальный";
+            }
+            else if (bodyMassIndex >= 25)
+            {
+                result = "ИМТ = " + bodyMassIndex + " - Выше нормального";
+            }
+            else
+            {
+                result = "Ошибка в расчетах, проверьте корректность данных ";
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ TestNORBIT/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNORBIT
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            IndexCalculation indexCalculation = new IndexCalculation();
+            indexCalculation.InputData();
+
+            Console.WriteLine("Нажмите на любую клавишу, чтобы выйти из программы");
+            Console.ReadLine();
+        }
+    }
+}
